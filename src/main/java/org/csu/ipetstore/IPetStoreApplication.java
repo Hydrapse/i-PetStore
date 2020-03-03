83cd8d8bfe1e61e19977e965fce046c2c751d7ab
Hydrapse <1041422509@qq.com>
2020-03-03 10:40:41 +0800

    Initialize Project

---
+++ src/main/java/org/csu/ipetstore/IPetStoreApplication.java
@@ -0,0 +1,13 @@
+package org.csu.ipetstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IPetStoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IPetStoreApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/csu/ipetstore/IPetStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package org.csu.ipetstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IPetStoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
